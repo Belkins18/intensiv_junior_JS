dee6c781a5c006d89d8d4cbf65a7ea2da5287318
Aleksandr Belibov <alexander.belibov@gmail.com>
2018-08-09 01:49:21 +0300

    ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ: day_1

--- homework-08.08.18/script.js
+++ homework-08.08.18/script.js
@@ -44,3 +44,36 @@ ${mainList.marketName}
 ${mainList.shopGoods}
 Мой бюджет на 1 день: ${mainList.budgetForDay()}
 `);
+
+let num = 33721;
+const number = (digits) => {
+    let getArray = (n) => {
+        n = String(n).split('').map(Number);
+        return n;
+    };
+
+    let arr = getArray(digits);
+
+    return arr.reduce(function(mult, current) {
+        return mult * current
+    });
+};
+
+const additionalAssignment = {
+    resultNumber: number(num),
+
+    powResultNumber: function () {
+        return Math.pow( this.resultNumber, 3);
+    },
+    twoDigitsPowResultNumber: function () {
+        return parseInt(/\d{2}/.exec( '' + this.powResultNumber())[0])
+    },
+};
+
+console.log(`
+ДОПОЛНИТЕЛЬНОЕ (УСЛОЖНЕННОЕ) ЗАДАНИЕ:
+    
+1) результат умножения цифр числа:                  |   ${additionalAssignment.resultNumber}
+2) возведение в степень 3:                          |   ${additionalAssignment.powResultNumber()}
+3) вывод на экран первых 2 цифр полученного чиса:   |   ${additionalAssignment.twoDigitsPowResultNumber()}
+`);
